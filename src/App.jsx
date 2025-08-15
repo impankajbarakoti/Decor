af82da151b555d96e36daf149503e7d226159393
Pankaj Barakoti <pankajbarakoti837@gmail.com>
2025-08-15 18:20:54 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,9 @@ import About from "./components/pages/About";
 import LatestAndSidebar from "./components/LatestAndSidebar";
 import Contactus from "./components/pages/Contactus";
 import Blogs from "./components/Blogs";
-// import BlogDetails from "./components/pages/Blogdetails";
 import BlogDetails from "./components/pages/Blogdetails";
+
+
 
 
 const App = () => {
--- src/components/pages/BlogDetails.jsx
+++ src/components/pages/BlogDetails.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { useLocation, useParams, useNavigate } from "react-router-dom";
 import BlogsData from "./BlogsData";
